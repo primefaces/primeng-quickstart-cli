b5329c9ac6ffb6dc2f84ff9eba189ebc8a3261b8
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-06-07 13:23:32 +0300

    v12 update

--- e2e/src/app.e2e-spec.ts
+++
@@ -1,9 +0,0 @@
-import { AppPage } from './app.po';
-
-describe('primeng-quickstart-cli App', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
-  });
-});
--- e2e/src/app.po.ts
+++
@@ -1,7 +0,0 @@
-import { browser, element, by } from 'protractor';
-
-export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-}
